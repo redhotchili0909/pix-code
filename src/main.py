736aa34d72be35b3e4d6c18b977706311c5d5f5c
Jun Park <junpark0909@gmail.com>
2024-04-18 13:33:55 -0400

    Changed encoder into a struct

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
-from encoder import text_to_binary, binary_to_png, create_png_from_binary
+from encoder import Encoder
 
-NAME = "assets/lorem.txt"
-binary_content = text_to_binary(NAME)
-FRANKENSTEIN = "assets/frankenstein.txt"
-binary_frankenstein = text_to_binary(FRANKENSTEIN)
-create_png_from_binary(binary_frankenstein, "frankenstein.png")
+bee = Encoder("assets/bee.txt")
+bee.create_png_from_binary("bee")
+bee.generate_video("bee", 1)
